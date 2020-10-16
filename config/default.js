c273299ec1d9012b484397ca8daf8b364ed129b1
guofangchao <guofangchao@daling.com>
2020-10-16 11:35:00 +0800

    增加数据库

--- config/default.js
+++ config/default.js
@@ -3,4 +3,19 @@
  */
 module.exports = {
     name: "nsp-user",
+    //开发环境数据库
+    db: {
+        host: "127.0.0.1",
+        port: "3306",
+        database: "database",
+        user: "data-user",
+        password: "1234567",
+        connectionLimit: 2,
+    },
+    //开发环境，普通redis配置
+    redis: {
+        port: 6379,
+        host: "127.0.0.1",
+        password: "123456",
+    },
 };
