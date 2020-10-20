46bbd5030e270ab7a9fdd6f8db6777acec779195
guofangchao <guofangchao@daling.com>
2020-10-20 18:55:21 +0800

    增加参数鉴定

--- src/middleware/check_server.ts
+++ src/middleware/check_server.ts
@@ -2,14 +2,14 @@ import { ictx } from "../extends";
 import { ErrorData } from "../service/utils";
 import config from "config";
 
-const pass_check = config.get("pass_check");
+const pass_check_cfg = config.get("pass_check");
 
 export function CheckServer() {
     return async function (ctx: ictx, next) {
         const { pass_check } = ctx.headers;
 
         //空参数是没登录
-        if (!pass_check) {
+        if (!pass_check || pass_check_cfg !== pass_check) {
             return (ctx.body = ErrorData("不是服务调用"));
         }
         await next();
